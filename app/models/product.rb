18d9643aad1e1f59a32c4bf2517aa458752f0cd7
peter <pkarella27@yahoo.com>
2017-07-16 17:05:27 -0700

    add ajax to submit form

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :order_items
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/monkey.jpg"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :image, content_type:/\Aimage\/.*\Z/
 
   validates :name, :description, :price, presence: true
 

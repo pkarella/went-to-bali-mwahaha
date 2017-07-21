29e2cae6bc55e04b6b58f9f6c40f26cca635bc2c
peter <pkarella27@yahoo.com>
2017-07-20 18:34:47 -0700

    final ajax implemented

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,18 @@ class ProductsController < ApplicationController
   before_filter :authorize, except: [:index, :show]
 
   def index
+
     @products = Product.all
     @order_item = current_order.order_items.new
   end
 
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to product_path }
+      format.js
   end
+end
 
   def new
     @product = Product.new
